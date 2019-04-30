e0e85ae31409c0651fc980e39c50bc3ba6d63445
Bogdanov Dmitry <bogdanovdi888@gmail.com>
2019-05-01 01:48:59 +0300

    Add ImageInterpreter
    Add ImageInterpreter

---
+++ NeuralNetwork/NeuralNetwork/ImageInterpreter.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralNetwork
+{
+    class CImageInterpreter
+    {
+        private List<Bitmap>[] m_imageLists;
+
+        public CImageInterpreter()
+        {
+            int index = 0;
+            m_imageLists = new List<Bitmap>[32];
+            DirectoryInfo letterDir = new DirectoryInfo(@"C:\Users\dbogdano\source\repos\NeuralNetwork\NeuralNetwork\Cyrillic");
+            foreach (var letter in letterDir.GetDirectories())
+            {
+                //Console.WriteLine("!!!!!!!!!!!!");
+                //Console.WriteLine(letter.FullName);
+                m_imageLists[index] = new List<Bitmap>();
+                foreach (var image in letter.GetFiles())
+                {
+                    if (image.FullName.EndsWith(".png"))
+                    {
+                        Bitmap tmpImage = new Bitmap(image.FullName);
+                        m_imageLists[index].Add(tmpImage);
+                    }
+                    //Console.WriteLine(image.FullName);
+                }
+            }
+            Console.WriteLine("End of ImageInterpreter initialize");
+        }
+
+        public List<double> GetImageValue(int iIndex, int jIndex)
+        {
+            List<double> value = new List<double>();
+            Bitmap bitmap = m_imageLists[iIndex][jIndex];
+            for (int i = 0; i < bitmap.Width; ++i)
+            {
+                for (int j = 0; j < bitmap.Height; ++j)
+                {
+                    double pixel = bitmap.GetPixel(i, j).R;
+                    value.Add(pixel);
+                }
+            }
+            return value;
+        }
+
+        public int GetLetterCount()
+        {
+            return m_imageLists.Count();
+        }
+
+        public int GetImageCount(int index)
+        {
+            return m_imageLists[index].Count();
+        }
+
+        public List<double> GetAnswer(int letterNumber)
+        {
+            List<double> answer = new List<double>();
+            for (int i = 0; i < m_imageLists.Count(); ++i)
+            {
+                if (i != letterNumber)
+                {
+                    answer.Add(0);
+                }
+                else
+                {
+                    answer.Add(1);
+                }
+            }
+            return answer;
+        }
+    }
+}
--- NeuralNetwork/NeuralNetwork/Program.cs
+++ NeuralNetwork/NeuralNetwork/Program.cs
@@ -10,20 +10,34 @@ namespace NeuralNetwork
     {
         static void Main(string[] args)
         {
-            CNeuralNetwork neuralNetwork = new CNeuralNetwork("XOR", 2, 5, 1);
-            for (int i = 0; i < 10000; ++i)
+            //CNeuralNetwork neuralNetwork = new CNeuralNetwork("XOR", 2, 5, 1);
+            //for (int i = 0; i < 10000; ++i)
+            //{
+            //    for (int j = 0; j <= 1; ++j)
+            //    {
+            //        for (int k = 0; k <= 1; ++k)
+            //        {
+            //            List<double> inputValue = new List<double> { j, k };
+            //            List<double> answer = new List<double> { j ^ k };
+            //            Console.WriteLine(answer[0] + " -- " + neuralNetwork.Run(inputValue, answer)[0]);
+            //        }
+            //    }
+            //}
+
+            //neuralNetwork.Save();
+
+            //Eat many memory
+            CNeuralNetwork neuralNetwork = new CNeuralNetwork("CyrilicRecognition", 21025, 30000, 32);
+            CImageInterpreter imageInterpreter = new CImageInterpreter();
+            for (int i = 0; i < imageInterpreter.GetLetterCount(); ++i)
             {
-                for (int j = 0; j <= 1; ++j)
+                for (int j = 0; j < imageInterpreter.GetImageCount(i); ++j)
                 {
-                    for (int k = 0; k <= 1; ++k)
-                    {
-                        List<double> inputValue = new List<double> { j, k };
-                        List<double> answer = new List<double> { j ^ k };
-                        Console.WriteLine(answer[0] + " -- " + neuralNetwork.Run(inputValue, answer)[0]);
-                    }
+                    neuralNetwork.Run(imageInterpreter.GetImageValue(i, j), imageInterpreter.GetAnswer(i));
                 }
             }
             neuralNetwork.Save();
+            Console.WriteLine("End of Run");
             Console.ReadKey();
         }
     }
